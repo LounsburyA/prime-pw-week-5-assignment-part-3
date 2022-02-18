f738c3e59453377c61e51f07ec72ea533b7254a8
Andrew Lounsbury <lounsburya@gmail.com>
2022-02-18 09:53:17 -0600

    wrote addToCollection function, took some notes on figuring it out

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,29 @@
 console.log('***** Music Collection *****')
 let collection = []
 
-fucntion addToCollection(title, artist, yearPublished){
-  
-}
+function addToCollection(title, artist, yearPublished){
 let album = {
-
+  title: title,
+  artist: artist,
+  yearPublished: yearPublished
 }
+
+  collection.push(album);
+  return album;
+}
+
+console.log(addToCollection('bugs', 'cat', 1995));
+  //let album = {title, artist, yearPublished}
+  // push album to Collection
+  // return album
+  //collection.push(title, artist, yearPublished);
+  //return true;
+
+console.log(collection);
+// let basket = [];
+// function addItem(item,cat, taco){
+//   basket.push(item, cat, taco);
+//   return true;
+// }
+// console.log(addItem('one', 'two', 'three'));
+// console.log(basket);
